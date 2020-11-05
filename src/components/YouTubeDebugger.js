90e860c808736edafe57ca9b46546dd7352cc7ea
Kat Tochowicz <ktochow1@binghamton.edu>
2020-11-05 14:25:54 +0100

    commit tree 318ba7d49a63e48cba62d1549db1e4b415e40f43

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,60 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+export default class YouTubeDebugger extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    handleBitrate = () => {
+        console.log(this.state)
+        
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+        console.log(this.state)
+    }
+
+    handleResolution = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <button 
+                className="bitrate"
+                onClick={this.handleBitrate}
+                >Bitrate
+                </button>
+
+                <button className="resolution" onClick={this.handleResolution}>Resolution</button>
+            </div>
+            
+
+            
+
+        )
+    }
+}
